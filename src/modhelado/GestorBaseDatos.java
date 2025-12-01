6865d7edf2d02f28590372405e6624fc54fbce68
GonzaloPDM <gonzalopantindm@gmail.com>
2025-12-01 13:06:18 +0100

    Package tablon completed and GestorBaseDatos class added

---
+++ src/modhelado/GestorBaseDatos.java
@@ -0,0 +1,12 @@
+package modhelado;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GestorBaseDatos {
+    public static void guardar(String datos) {}
+
+    public static List<Object> consultar(String consulta) {
+        return Collections.emptyList();
+    }
+}
