60b2a008372ebd357ca990327afdbd008a96ee0a
juan33133 <juancarballalfdez3@gmail.com>
2025-12-09 17:10:23 +0100

    Chat privado coment y gestor import

--- src/modhelado/chat/ChatPrivado.java
+++ src/modhelado/chat/ChatPrivado.java
@@ -5,6 +5,7 @@ import modhelado.usuario.Usuario;
 import modhelado.usuario.conexion.Aceptada;
 import modhelado.usuario.conexion.Conexion;
 
+
 public class ChatPrivado extends Chat {
 
 	/**
@@ -16,10 +17,11 @@ public class ChatPrivado extends Chat {
 	public ChatPrivado(Usuario usuario1, Usuario usuario2, String fecha) {
 		super(usuario1, fecha);
 		// Constraint: ChatPrivadoConexionAceptada
+		/** HAY QUE REVISAR COMO HACEMOS LAS PETICIONES
 		assert !(GestorBaseDatos.consultar("SELECT emisor FROM CONEXIONES WHERE (emisor = '" + usuario1.getUsername() + "' AND receptor = '" + usuario2.getUsername()
 				+ "') OR (emisor = '" + usuario2.getUsername()+ "' AND receptor = '" + usuario1.getUsername() + "')").getFirst() instanceof Conexion conexion &&
 				conexion.getEstado().equals(Aceptada.aceptada()));
-
+		*/
 		// Constraint: chatPrivadoUsuariosDiferentes
 		assert !usuario1.equals(usuario2);
 
